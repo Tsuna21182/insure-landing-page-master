502aa1f8e9ec8f2c82b45d35171ddcfde99203c6
Tsuna21182 <tsunadelanochecer@gmail.com>
2025-02-08 12:17:38 -0500

    bg pendiente

--- src/components/Section/FindWork.tsx
+++ src/components/Section/FindWork.tsx
@@ -2,7 +2,7 @@ import Btn from "../Btn";
 
 function FindWork() {
   return (
-    <section className="bg-DarkViolet bg-[url(/images/bg-pattern-how-we-work-mobile.svg)] bg-no-repeat bg-[13rem_-1rem] flex flex-col justify-center items-center text-center p-10 mx-5 my-20 h-80 xl:flex-row xl:justify-between xl:p-20 xl:h-60 xl:text-left xl:mx-25">
+    <section className="bg-DarkViolet bg-[url(/images/bg-pattern-how-we-work-mobile.svg)] bg-no-repeat bg-[13rem_-1rem] md:bg-[url(/images/bg-pattern-how-we-work-desktop.svg)] md:bg-right flex flex-col justify-center items-center text-center p-10 mx-5 my-20 h-80 xl:flex-row xl:justify-between xl:p-20 xl:h-60 xl:text-left xl:mx-25">
       <h2 className="font-display text-VeryLightGray text-4xl my-5 xl:w-80">
         Find out more about how we work
       </h2>
